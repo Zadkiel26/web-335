50fc16e1e4956aed748b977de04454716bfa4120
Zadkiel <zadkiel679@gmail.com>
2024-01-21 15:53:58 -0500

    Week 2 Assignments Added

---
+++ week-2/RodriguezAlvarado_myworld.py
@@ -0,0 +1,8 @@
+"""
+    Title: RodriguezAlvarado_myworld.py
+    Author: Zadkiel Rodriguez Alvarado
+    Date: 01/21/2024
+    Description: Print "You are now in Rodriguez Alvarado's world!"
+"""
+last_name = "Rodriguez Alvarado"
+print(f"You are now in {last_name}'s world!")
